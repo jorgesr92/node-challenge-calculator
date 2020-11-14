5158b32ba3fef97b6147d0e72ae58b1305232c78
jorgesr92 <jorgesr92@github.com>
2020-11-14 11:32:41 +0100

    homework finished

--- server.js
+++ server.js
@@ -3,7 +3,12 @@ const app = express();
 
 const port = 3000;
 
-app.get('/Add', (req, res) => {
+const getNumbers = (numbers) => {
+    console.log(numbers);
+}
+
+/* app.get('/Add', (req, res) => {
+    getNumbers(req.query);
     const num1 = req.query.value1;
     const num2 = req.query.value2;
     console.log(`el cliente quiere sumar los numeros ${num1} y ${num2}`);
@@ -30,6 +35,36 @@ app.get('/Divide', (req, res) => {
     console.log(`el cliente quiere dividir los numeros ${num1} y ${num2}`);
     res.send(`La división de los numeros ${num1} y ${num2} es: ${parseInt(num1) / parseInt(num2)}`);
 });
+ */
+
+const myLogger = (req, res, next) => {
+    const visitTime = new Date();
+    console.log(`visited ${req.url} at ${visitTime.toLocaleString()}`);
+    next();
+};
+app.use(myLogger);
+
+ const getResult = (oper, value1, value2) => {
+    if (oper === "add") {
+        return +value1 + +value2;
+    } else if (oper ==="substract") {
+        return +value1 - +value2;
+    }else if (oper ==="multiply") {
+        return +value1 - +value2;
+    }else if (oper ==="divide") {
+        return +value1 - +value2;
+    };
+ };
+
+app.get('/:operation/:value1/:value2', (req, res) => {
+    const operation = req.params.operation;
+    const num1 = req.params.value1;
+    const num2 = req.params.value2;
+    let result = getResult(operation, num1, num2);
+    console.log(`el cliente quiere ${operation} los numeros ${num1} y ${num2}`);
+    res.send(`La ${operation} de los numeros ${num1} y ${num2} es: ${result.toString()}`);
+});
+
 
 app.listen(port, () => {
   console.log("Server is listening on port 3000. Ready to accept requests!");
